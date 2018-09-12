f9ef5775af9553afcf805e1db7584246bfef9e0b
Paul Hulett <hulett@squareup.com>
2018-09-11 17:27:00 -0700

    mustache test

--- public/index.php
+++ public/index.php
@@ -5,7 +5,12 @@ if (!file_exists(__DIR__ . '/../vendor/')) {
 }
 require_once __DIR__ . '/../vendor/autoload.php';
 $m = new Mustache_Engine();
-$values = [];
+$values = [
+    'name' => 'fellow human',
+    'value' => 7.11,
+    'in_ca' => true,
+    'taxed_value' => 7
+];
 $template = file_get_contents(__DIR__ . '/home.html');
 echo $m->render($template, $values);
 //SquareServerlessQuickstart\Foo::output();
